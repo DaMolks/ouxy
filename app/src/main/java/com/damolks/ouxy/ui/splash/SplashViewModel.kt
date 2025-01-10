eee6077d378cb97be9a0f6a80e977640cb607fcb
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-10 17:19:39 +0100

    Remove splash screen delay

--- app/src/main/java/com/damolks/ouxy/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/damolks/ouxy/ui/splash/SplashViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashViewModel : ViewModel() {
@@ -18,7 +17,6 @@ class SplashViewModel : ViewModel() {
 
     private fun checkTechnicianProfile() {
         viewModelScope.launch {
-            delay(1500) // Délai pour l'effet visuel
             // TODO: Vérifier si un technicien existe
             _navigationAction.value = SplashNavigationAction.NavigateToTechnicianSetup
         }
