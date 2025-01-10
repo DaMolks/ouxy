68a667a58eea8059e3ddc0e6207493702be206b5
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-10 21:49:35 +0100

    Add: Hilt Application and AppModule

--- app/src/main/java/com/damolks/ouxy/OuxyApplication.kt
+++ app/src/main/java/com/damolks/ouxy/OuxyApplication.kt
@@ -2,7 +2,9 @@ package com.damolks.ouxy
 
 import android.app.Application
 import com.damolks.ouxy.data.database.OuxyDatabase
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class OuxyApplication : Application() {
     val database: OuxyDatabase by lazy { OuxyDatabase.getDatabase(this) }
 }
