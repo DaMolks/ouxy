edf03629aadac49e581249643f3277d329e6faaa
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-10 17:17:23 +0100

    Add: Basic TechnicianSetupFragment implementation

---
+++ app/src/main/java/com/damolks/ouxy/ui/technician/TechnicianSetupViewModel.kt
@@ -0,0 +1,7 @@
+package com.damolks.ouxy.ui.technician
+
+import androidx.lifecycle.ViewModel
+
+class TechnicianSetupViewModel : ViewModel() {
+    // TODO: Implémenter la logique de configuration du technicien
+}
